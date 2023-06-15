374824ff8374ecb1d56e37ebc9dccf5c31521b28
22mh1a05f6 <119723846+22mh1a05f6@users.noreply.github.com>
2023-06-15 10:08:45 +0530

    #include <stdio.h> int isPronicNumber(int num) {     for (int i = 1; i * (i + 1) <= num; i++) {         if (i * (i + 1) == num)             return 1;     }
        return 0; 
    }
    
    
    int main() {
        int num;
        scanf("%d", &num);
        if (isPronicNumber(num))
            printf("YES");
        else
            printf("NO");
        
        return 0;
    }

---
+++ Pronic_Number_or_Heteromecic_Number.c
@@ -0,0 +1,21 @@
+#include <stdio.h> 
+int isPronicNumber(int num) {
+    for (int i = 1; i * (i + 1) <= num; i++) {
+        if (i * (i + 1) == num)
+            return 1; 
+    }
+    
+    return 0; 
+}
+
+
+int main() {
+    int num;
+    scanf("%d", &num);
+    if (isPronicNumber(num))
+        printf("YES");
+    else
+        printf("NO");
+    
+    return 0;
+}
